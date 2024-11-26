3e741d4cd5c1f2307aa37654983cb957433f5310
DanRows <39061444+DanRows@users.noreply.github.com>
2024-11-26 16:04:04 -0300

    update

--- app/main.py
+++ app/main.py
@@ -1,9 +1,3 @@
-# app/main.py (ampliado)
-import sys
-import os
-
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import streamlit as st
 from config.configuration import Configuration
